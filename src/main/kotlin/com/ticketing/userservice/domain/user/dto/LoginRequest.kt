7917d505cd84b94bafe9b11a362e5ce679efd019
sanghyeop <xoxoxmin1268@naver.com>
2025-07-02 17:41:50 +0900

    사용자 회원가입 swagger excaple 변경

--- src/main/kotlin/com/ticketing/userservice/domain/user/dto/LoginRequest.kt
+++ src/main/kotlin/com/ticketing/userservice/domain/user/dto/LoginRequest.kt
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema
  */
 @Schema(description = "사용자 로그인을 위한 요청 데이터 모델")
 class LoginRequest (
-    @Schema(description = "이메일", example = "")
+    @Schema(description = "이메일", example = "test@naver.com")
     val email: String,
 
     @Schema(description = "비밀번호", example = "password1234!")
